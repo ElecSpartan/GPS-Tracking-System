253e38a592f1a5354997050dd23d4beffb02405d
John-Mamdouh <105706906+John-Mamdouh@users.noreply.github.com>
2023-05-12 22:23:19 +0300

    Added UART2 functions

---
+++ uart.h
@@ -0,0 +1,18 @@
+#ifndef _TM4C_H
+#define _TM4C_H
+#include "tm4c123gh6pm.h"
+#endif
+#ifndef _STDINT_H
+#define _STDINT_H
+#include <stdint.h>
+#endif
+#ifndef _UART_H
+#define _UART_H
+
+void uart2_init(unsigned clk,unsigned baudrate);
+
+void uart2_send_byte(uint8_t c);
+
+uint8_t uart2_read_byte(void);
+
+#endif
