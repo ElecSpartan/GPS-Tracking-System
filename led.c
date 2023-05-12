f95ef57ebddcfa318a868eef4d9d52dcb95a784c
MohamedHusseinn <130403650+MohamedHusseinn@users.noreply.github.com>
2023-05-12 22:44:12 +0300

    Updated led functions for GPS

--- led.c
+++ led.c
@@ -9,3 +9,18 @@ void RGB_clear(char mask){
     mask &= 0X0E;
     GPIO_PORTF_DATA_R &= ~mask;
 }
+
+void RGB(char colour){
+	GPIO_PORTF_DATA_R &= ~RGB_LED;			// Stop all active LEDs
+	GPIO_PORTF_DATA_R |= colour;			// Turn on this led
+}
+
+void distance_indicator(int current_distance){
+	if(current_distance == 1){
+		RGB(GREEN_LED);
+	} else if(current_distance <= 5){
+		RGB(GREEN_LED | RED_LED);
+	} else {
+		RGB(RED_LED);
+	}
+}
